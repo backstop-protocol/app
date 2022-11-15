b2ef13d1dea5a8edc8457534772086821ede8330
shmuel <shmuel.disraeli@gmail.com>
2022-11-15 15:22:01 +0200

    footer tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
-  return <footer className="container">
-    <div style={{padding: 'var(--spacing)', display: 'flex' , justifyContent: 'center'}}>
+  return <footer style={{    
+    marginTop: '40px',
+    background: 'var(--card-background-color)',
+    boxShadow: 'var(--card-box-shadow)'
+    }}>
+    <div className="container" style={{padding: 'var(--spacing)', display: 'flex' , justifyContent: 'center'}}>
       <a href="/terms"><small>terms & conditions</small></a>
     </div>
   </footer>
